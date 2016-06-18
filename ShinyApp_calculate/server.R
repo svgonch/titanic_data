07694f178f0e1a1560fbfcbae634eb6f9fdb9989
Stepan Goncharov <gostevl@hotmail.com>
2016-06-19 01:08:14 +0300

    Adding shiny app

---
+++ ShinyApp_calculate/server.R
@@ -0,0 +1,14 @@
+library(HistData)
+data(Galton)
+
+shinyServer(  
+      function(input, output) {    
+            output$newHist <- renderPlot({      
+                  hist(Galton$child, xlab='child height', col='lightblue',main='Histogram')      
+                  mu <- input$mu      
+                  lines(c(mu, mu), c(0, 200),col="red",lwd=5)      
+                  mse <- mean((Galton$child - mu)^2)      
+                  text(63, 150, paste("mu = ", mu))      
+                  text(63, 140, paste("MSE = ", round(mse, 2)))      
+            })      }
+)
